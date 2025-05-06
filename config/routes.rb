49b16b819e6397e56a57deb465da6b6425745787
varshnimurali <vmurali0@chicagobooth.edu>
2025-05-06 19:36:29 +0000

    save changes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get("/directors", { :controller => "directors", :action => "index" })
   get("/directors/:path_id", { :controller => "directors", :action => "show" })
   post("/insert_director_record", { :controller => "directors", :action => "create" })
+  params = { :controller => "directors", :action => "create" }
   get("/delete_director/:an_id", { :controller => "directors", :action => "destroy" })
   post("/modify_director_record/:the_id", { :controller => "directors", :action => "update" })
 
